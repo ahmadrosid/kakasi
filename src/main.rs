cde0adf8265379b7b948fc644d9d8343c4caa68f
Ahmad Rosid <alahmadrosid@gmail.com>
2022-06-18 11:31:51 +0700

    fix: move cli project to root

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod commands;
+mod command_prelude;
+
+use command_prelude::*;
+
+fn main() -> CliResult {
+    let mut config = Config{};
+    let args = match cli::parse().try_get_matches() {
+        Ok(args) => args,
+        Err(e) => {
+            return Err(e.into());
+        }
+    };
+
+    if let Some((cmd, args)) = args.subcommand() {
+        if let Some(cm) = commands::builtin_exec(cmd) {
+            let _ = cm(&mut config, args);
+        }
+    } else {
+        cli::print_help();
+    }
+
+    Ok(())
 }
